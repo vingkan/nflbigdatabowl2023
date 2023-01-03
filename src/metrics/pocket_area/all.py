e6824102602583926dc22295a77c804524533662
Alek Popovic <apopovic@hawk.iit.edu>
2023-01-03 20:45:44 +0000

    fixed test and typing

--- src/metrics/pocket_area/all.py
+++ src/metrics/pocket_area/all.py
@@ -5,9 +5,10 @@ from src.metrics.pocket_area.passer_radius_area import get_passer_radius_area
 from src.metrics.pocket_area.pocket_pb_ch_area import (
     get_passBlocker_convexHull_area,
 )
+from src.metrics.pocket_area.rushers_pocket_area import rushers_pocket_area
 
 POCKET_AREA_METHODS: Dict[str, PocketAreaFunction] = {
     "passer_radius": get_passer_radius_area,
     "blocker_convex_hull": get_passBlocker_convexHull_area,
-    "rushers_pocket_area": rushers_pocket_area
+    "rushers_pocket_area": rushers_pocket_area,
 }
