fe700bddea10dcdd04f5cf6849cefe1cec571628
Vinesh Kannan <vingkan@gmail.com>
2023-01-07 16:31:14 -0600

    Download processed data from Kaggle. (#35)

--- run.sh
+++ run.sh
@@ -21,8 +21,12 @@ download_kaggle_data () {
     # Activate Python virtual environment
     source .venv/bin/activate
     # Create necessary folders, if they do not exist
+    rm -rf data/zipped
+    rm -rf data/raw
+    rm -rf data/processed
     mkdir -p data/zipped
     mkdir -p data/raw
+    mkdir -p data/processed
     # Refresh GitPod workspace environment variables
     eval $(gp env -e)
     # Download data using Kaggle credentials from user environment variables
@@ -31,6 +35,8 @@ download_kaggle_data () {
     unzip data/zipped/nfl-big-data-bowl-2023.zip -d data/raw
     # Delete zipped data
     rm -rf data/zipped
+    # Download processed data run by pipeline on Kaggle
+    kaggle kernels output vingkan/process-pocket-area-data -p data/processed
 }
 
 start_jupyter_server () {
@@ -41,6 +47,10 @@ start_jupyter_server () {
     GITPOD_DOMAIN=$(gp url "$JUPYTER_PORT")
     # Start Jupyter server (without token)
     jupyter notebook --port="$JUPYTER_PORT" --NotebookApp.allow_origin="$GITPOD_DOMAIN" --NotebookApp.token=""
+}
+
+run_pipeline () {
+    python3 src/pipeline/flows/run.py
 }
 
 # Run commands based on chosen shortcut and options
@@ -54,8 +64,14 @@ if [ "$1" == "install-project" ]; then
 elif [ "$1" == "install-background" ]; then
     # Download Kaggle data
     download_kaggle_data
-    # Install system packages
-    install_system_packages
+    # Install system packages (skip for now)
+    # install_system_packages
+    # Run Pipeline (on small dataset)
+    run_pipeline
+
+# Download Kaggle data
+elif [ "$1" == "download-kaggle-data" ]; then
+    download_kaggle_data
 
 # Install only Python requirements
 elif [ "$1" == "install-python-requirements" ]; then
@@ -64,6 +80,11 @@ elif [ "$1" == "install-python-requirements" ]; then
 # Run Jupyter notebook server
 elif [ "$1" == "jupyter" ]; then
     start_jupyter_server
+
+
+# Run Pipeline (on small dataset)
+elif [ "$1" == "pipeline" ]; then
+    run_pipeline
 
 else
     echo "No run shortcut found for: '$1'"
