f62f738c95d6a6d0f700a99c15a05d71e2f4ba8d
Vinesh Kannan <vingkan@gmail.com>
2023-01-09 21:18:29 +0000

    Add more windows.

--- src/visualization/scatter.py
+++ src/visualization/scatter.py
@@ -9,12 +9,50 @@ from matplotlib.offsetbox import AnnotationBbox, OffsetImage, TextArea
 from src.visualization.helpers import unsnake
 from src.visualization.logos import LogoMap
 
+# https://teamcolorcodes.com/nfl-team-color-codes/
+color_for_team = {
+    "ARI": (151, 35, 63),
+    "ATL": (167, 25, 48),
+    "BAL": (26, 25, 95),
+    "BUF": (0, 51, 141),
+    "CAR": (0, 133, 202),
+    "CHI": (11, 22, 42),
+    "CIN": (251, 79, 20),
+    "CLE": (49, 29, 0),
+    "DAL": (0, 53, 148),
+    "DEN": (251, 79, 20),
+    "DET": (0, 118, 182),
+    "GB": (24, 48, 40),
+    "HOU": (3, 32, 47),
+    "IND": (0, 44, 95),
+    "JAX": (16, 24, 32),
+    "KC": (227, 24, 55),
+    "LAC": (0, 128, 198),
+    "LA": (0, 53, 148),
+    "MIA": (0, 142, 151),
+    "MIN": (79, 38, 131),
+    "NE": (0, 34, 68),
+    "NO": (211, 188, 141),
+    "NYG": (1, 35, 82),
+    "NYJ": (18, 87, 64),
+    "LV": (0, 0, 0),
+    "PHI": (0, 76, 84),
+    "PIT": (255, 182, 18),
+    "SF": (170, 0, 0),
+    "SEA": (0, 34, 68),
+    "TB": (213, 10, 10),
+    "TEN": (75, 146, 219),
+    "WAS": (90, 20, 20),
+    "LV": (165, 172, 175),
+}
+
 
 def get_team_scatter_ranker(df_play_info: pd.DataFrame) -> Callable:
     def ranker(method: str, formation: str):
         query = f"method == '{method}' and offenseFormation in ({formation})"
         df = pd.DataFrame(df_play_info.query(query))
         df["team"] = df["possessionTeam"]
+        df["opponent"] = df["defensiveTeam"]
         df["time_in_pocket"] = df["frame_end"] - df["frame_start"]
         df["is_sack"] = (df["passResult"] == "S").astype(int)
         aggregations = {
@@ -23,21 +61,35 @@ def get_team_scatter_ranker(df_play_info: pd.DataFrame) -> Callable:
             "median_time_in_pocket": ("time_in_pocket", np.median),
             "sack_rate": ("is_sack", np.mean),
         }
+
+        df_opp = df.groupby(["opponent"]).agg(**aggregations).reset_index()
         df_gp = df.groupby(["team"]).agg(**aggregations).reset_index()
-        df_gp["median_time_in_pocket"] = df_gp["median_time_in_pocket"] / 10.0
-        df_out = df_gp.sort_values(
+
+        df_out = df_gp.merge(
+            df_opp,
+            left_on=["team"],
+            right_on="opponent",
+            suffixes=["", "_opponent"],
+            how="left",
+        )
+
+        df_out["median_time_in_pocket"] = df_out["median_time_in_pocket"] / 10.0
+        df_out["median_time_in_pocket_opponent"] = (
+            df_out["median_time_in_pocket_opponent"] / 10.0
+        )
+        df_out = df_out.sort_values(
             by=["median_area"], ascending=False
         ).reset_index(drop=True)
         df_out["rank"] = np.arange(1, len(df_out) + 1, 1)
+        out_keys = ["rank", "team"]
         out_cols = [
-            "rank",
-            "team",
             "plays",
             "median_area",
             "median_time_in_pocket",
             "sack_rate",
         ]
-        return df_out[out_cols]
+        out_cols_opp = [f"{col}_opponent" for col in out_cols]
+        return df_out[out_keys + out_cols + out_cols_opp]
 
     return ranker
 
@@ -91,3 +143,56 @@ def get_team_scatter_plotter(
         fig.set_size_inches(8, 8)
 
     return plot
+
+
+def plot_rankings(df_in, team_logos, area_col, ascending):
+    df_team_ranking = pd.DataFrame(df_in).sort_values(
+        by=area_col, ascending=ascending
+    )
+    df_team_ranking["rank"] = np.arange(1, len(df_team_ranking) + 1, 1)
+    fig, ax = plt.subplots(1, 1)
+    pal = []
+    for tup in df_team_ranking[["team", "rank", area_col]].itertuples(
+        index=False
+    ):
+        team, x, y = tup
+        if team_logos is not None:
+            color = color_for_team[team]
+            pal.append(color)
+            team_logo = team_logos[team]
+            logo = AnnotationBbox(
+                OffsetImage(team_logo, zoom=0.35), xy=(x, y), frameon=False
+            )
+            ax.add_artist(logo)
+        text = AnnotationBbox(
+            TextArea(team),
+            xy=(x, y),
+            xybox=(0, 10),
+            xycoords="data",
+            boxcoords="offset points",
+            fontsize=2,
+            frameon=False,
+        )
+        ax.add_artist(text)
+    pal = np.array(pal) / 255.0
+    bar_width = 1
+    ax.bar(
+        x=df_team_ranking["rank"],
+        height=df_team_ranking[area_col],
+        width=bar_width,
+        color=pal,
+    )
+    ax.axhline(
+        df_team_ranking[area_col].max(), linestyle="--", color="lightgray"
+    )
+    ax.axhline(
+        df_team_ranking[area_col].mean(), linestyle="--", color="lightgray"
+    )
+    ax.axhline(
+        df_team_ranking[area_col].min(), linestyle="--", color="lightgray"
+    )
+    ax.set_ylim(0, 1.2 * df_team_ranking[area_col].max())
+    ax.set_xticks(np.arange(1, len(df_team_ranking) + 1, 1))
+    ax.set_xlabel("Team Rank")
+    fig.set_size_inches(14, 4)
+    return
